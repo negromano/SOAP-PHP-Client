211d1840a7fd4a1b6c39c6f31203d5f99ee17c87
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2018-10-29 03:25:56 -0500

    Fixed some bug on nusoap, added create petition schema

--- client.php
+++ client.php
@@ -6,6 +6,25 @@
  *
  */
 
-require_once('lib/nusoap.php');
+require_once('./lib/nusoap.php');
+$wsdl = "http://desktop-3fvpk7n:7101/ServerFootball-ServerFootball-context-root/ServicesPort?WSDL";
+$client = new nusoap_client($wsdl, 'wsdl');
+$err = $client->getError();
+if ($err) {
+   // Display the error
+   echo '<h2>Constructor error</h2>' . $err;
+   // At this point, you know the call that follows will fail
+   exit();
+}
+
+
+// Call the hello method
+$futbolista = array(
+  "arg0" => array("club" => "Tolima" )
+);
+$result1=$client->call('create', $futbolista);
+echo( $result1['return']);
+echo "hello World";
+
 
 ?>
